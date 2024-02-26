d36b927ac2c0df189638ab1557e5dde5c857caa2
bryan5243 <chuchei5243@gmail.com>
2024-02-26 12:35:01 -0500

    culminado

--- admin/pre_inscripcion.php
+++ admin/pre_inscripcion.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+include_once '../model/conexion.php'; 
+?>
 <html lang="es">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
--- web2/model/conexion.php
+++ web2/model/conexion.php
@@ -1,18 +1,19 @@
 <?php
 
-$host = 'monorail.proxy.rlwy.net';
-$port = '54739'; // Puerto por defecto de MySQL
-$dbname = 'arias'; // Nombre de la base de datos MySQL
-$user = 'root'; // Nombre de usuario de MySQL
-$password = 'dC6A24GC53FG2E1-B5BFh3fF511GFgGD'; // Contraseña de MySQL
+    $host = 'localhost';
+    $port = '3306'; // Puerto por defecto de MySQL
+    $dbname = 'arias'; // Nombre de la base de datos MySQL
+    $user = 'root'; // Nombre de usuario de MySQL
+    $password = ''; // Contraseña de MySQL
 
-// Conexión a la base de datos
-try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
-    // Configuración para manejar errores y excepciones de PDO
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
-    exit(); // Si hay un error, termina el script
-}
-return $pdo;
+    // Conexión a la base de datos
+    try {
+        $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
+        // Configuración para manejar errores y excepciones de PDO
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Error de conexión: " . $e->getMessage();
+        exit(); // Si hay un error, termina el script
+    }
+    return $pdo;
+?>
