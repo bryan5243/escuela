0239e55d6b8176c7d8592495055ff6654068013e
chuchei <chuchei5243@gmail.com>
2024-05-15 19:30:59 -0500

    pruebas zeabur

--- web2/model/conexion.php
+++ web2/model/conexion.php
@@ -1,19 +1,18 @@
 <?php
 
-    $host = 'localhost';
-    $port = '3306'; // Puerto por defecto de MySQL
-    $dbname = 'arias'; // Nombre de la base de datos MySQL
-    $user = 'root'; // Nombre de usuario de MySQL
-    $password = ''; // Contraseña de MySQL
+$host = 'sfo1.clusters.zeabur.com';
+$port = '32631'; // Puerto por defecto de MySQL
+$dbname = 'zeabur'; // Nombre de la base de datos MySQL
+$user = 'root'; // Nombre de usuario de MySQL
+$password = '9s03IS8RZWr5hXz24JL6HyBtV1cN7mlP'; // Contraseña de MySQL
 
-    // Conexión a la base de datos
-    try {
-        $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
-        // Configuración para manejar errores y excepciones de PDO
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo "Error de conexión: " . $e->getMessage();
-        exit(); // Si hay un error, termina el script
-    }
-    return $pdo;
-?>
+// Conexión a la base de datos
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    // Configuración para manejar errores y excepciones de PDO
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+    exit(); // Si hay un error, termina el script
+}
+return $pdo;
